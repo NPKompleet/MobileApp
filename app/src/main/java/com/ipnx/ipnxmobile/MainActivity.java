a25df5b6d6fe71aadd88cfee00b2b8d236ac3dbf
Philip <filip2unine@yahoo.co.uk>
2019-02-25 15:10:40 +0100

    first commit after sign in and front page created

---
+++ app/src/main/java/com/ipnx/ipnxmobile/MainActivity.java
@@ -0,0 +1,20 @@
+package com.ipnx.ipnxmobile;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openMainView(View view){
+        Intent i = new Intent(this, ServicePlanActivity.class);
+        startActivity(i);
+    }
+}
