e1568f7bf2b939cde6ebce1503084195d93f1f46
Philip <filip2unine@yahoo.co.uk>
2019-07-03 13:47:01 +0100

    added navigation drawer to services menu

--- app/src/main/java/com/ipnx/ipnxmobile/ServicesMenuActivity.java
+++ app/src/main/java/com/ipnx/ipnxmobile/ServicesMenuActivity.java
@@ -1,9 +1,13 @@
 package com.ipnx.ipnxmobile;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +20,7 @@ import com.ipnx.ipnxmobile.adapters.InternetServiceAdapter;
 import com.ipnx.ipnxmobile.adapters.TelephonyServiceAdapter;
 import com.ipnx.ipnxmobile.models.requests.LoginRequestValues;
 import com.ipnx.ipnxmobile.models.responses.login.LoginResponse;
+import com.ipnx.ipnxmobile.wifianalyzer.WifiAnalyzerActivity;
 
 import java.util.Date;
 
@@ -23,13 +28,25 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import de.hdodenhof.circleimageview.CircleImageView;
 
+import static com.ipnx.ipnxmobile.utils.ApplicationUtils.APP_URL;
 import static com.ipnx.ipnxmobile.utils.ApplicationUtils.EXTRA_KEY_LOGIN;
 import static com.ipnx.ipnxmobile.utils.ApplicationUtils.EXTRA_KEY_RESPONSE;
+import static com.ipnx.ipnxmobile.utils.ApplicationUtils.EXTRA_KEY_USERNAME;
+import static com.ipnx.ipnxmobile.utils.ApplicationUtils.FACEBOOK_URL;
+import static com.ipnx.ipnxmobile.utils.ApplicationUtils.INSTAGRAM_URL;
+import static com.ipnx.ipnxmobile.utils.ApplicationUtils.TWITTER_URL;
+import static com.ipnx.ipnxmobile.utils.ApplicationUtils.WHATSSAPP_URL;
 import static com.ipnx.ipnxmobile.utils.ApplicationUtils.userProfile;
 
 public class ServicesMenuActivity extends AppCompatActivity {
     public final String ProfilePREFERENCES = "ProfilePrefs" ;
     public final String PictureLocation = "ProfilePictureLocation";
+
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawer;
+
+    @BindView(R.id.nav_view)
+    NavigationView navigationView;
 
     @BindView(R.id.internetServiceListView)
     ListView internetServiceListView;
@@ -102,6 +119,16 @@ public class ServicesMenuActivity extends AppCompatActivity {
         sharedpreferences = getSharedPreferences(ProfilePREFERENCES, Context.MODE_PRIVATE);
         loadProfilePicture();
 
+        View headerView = navigationView.getHeaderView(0);
+        TextView navFullName = headerView.findViewById(R.id.nav_full_name);
+        TextView navEmail = headerView.findViewById(R.id.nav_email);
+        navFullName.setText(userProfile.getFullName());
+        navEmail.setText(userProfile.getCustomerNumber());
+
+    }
+
+    public void onMenuClicked(View view) {
+        drawer.openDrawer(GravityCompat.START, true);
     }
 
     public void onMenuButtonClicked(View view){
@@ -142,7 +169,67 @@ public class ServicesMenuActivity extends AppCompatActivity {
         }
     }
 
-    public void onBackPressed(View view){
-        onBackPressed();
+    public void onNavItemSelected(View view) {
+        // Handle navigation view item clicks here.
+        int id = view.getId();
+        Intent i;
+        switch (id){
+            case R.id.menu_wifi_analyzer:
+                i = new Intent(this, WifiAnalyzerActivity.class);
+                startActivity(i);
+                break;
+            case R.id.menu_profile:
+                i = new Intent(this, ProfileActivity.class);
+                startActivity(i);
+                break;
+            case R.id.menu_password:
+                i = new Intent(this, ChangePasswordActivity.class);
+                i.putExtra(EXTRA_KEY_USERNAME, userProfile.getUserName());
+                startActivity(i);
+                break;
+            case R.id.menu_choose_plan:
+                finish();
+                break;
+            case R.id.menu_payment_history:
+                i = new Intent(this, TransactionHistoryActivity.class);
+                startActivity(i);
+                break;
+            case R.id.menu_feedback:
+                i = new Intent(this, FeedbackActivity.class);
+                startActivity(i);
+                break;
+            case R.id.menu_share:
+                Intent shareIntent = new Intent();
+                shareIntent.setAction(Intent.ACTION_SEND);
+                shareIntent.setType("text/plain");
+                shareIntent.putExtra(Intent.EXTRA_TEXT, "Hello. Download ipNX Mobile app from here: " + APP_URL);
+                startActivity(shareIntent);
+                break;
+            case R.id.menu_faq:
+                i = new Intent(this, FAQActivity.class);
+                startActivity(i);
+                break;
+            case R.id.menu_notification:
+                i = new Intent(this, PushNotificationActivity.class);
+                startActivity(i);
+                break;
+            case R.id.menu_facebook:
+                i = new Intent(Intent.ACTION_VIEW, Uri.parse(FACEBOOK_URL));
+                startActivity(i);
+                break;
+            case R.id.menu_twitter:
+                i = new Intent(Intent.ACTION_VIEW, Uri.parse(TWITTER_URL));
+                startActivity(i);
+                break;
+            case R.id.menu_instagram:
+                i = new Intent(Intent.ACTION_VIEW, Uri.parse(INSTAGRAM_URL));
+                startActivity(i);
+                break;
+            case R.id.menu_whatsapp:
+                i = new Intent(Intent.ACTION_VIEW, Uri.parse(WHATSSAPP_URL));
+                startActivity(i);
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
     }
 }
